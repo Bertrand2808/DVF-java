967b8f3a3ae3c2eed5ed0cb500016e687dd5df7b
Bertrand2808 <bertrand2808@icloud.com>
2023-12-21 10:14:42 +0100

    first commit

---
+++ src/main/java/fr/esgi/dvf/DvfApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.dvf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvfApplication {
+	// Main
+	public static void main(String[] args) {
+		SpringApplication.run(DvfApplication.class, args);
+	}
+
+}
